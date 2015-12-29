3243bcfae2ef4ba1e6cd723f7caa69db8eea6296
gjimenez523 <gabe.jimenez@outlook.com>
2015-12-29 14:47:18 -0700

    Initial - Update Branding

--- public/main.js
+++ public/main.js
@@ -229,7 +229,7 @@ $(function() {
   socket.on('login', function (data) {
     connected = true;
     // Display the welcome message
-    var message = "Welcome to Socket.IO Chat – ";
+    var message = "StyleRescue Chat";
     log(message, {
       prepend: true
     });
